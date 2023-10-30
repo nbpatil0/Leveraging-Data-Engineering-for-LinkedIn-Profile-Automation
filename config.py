b200ef513389598f4e1864c1e748bd44a147017f
Neelakanta Gouda Patil <nbgouda111@gmail.com>
2023-10-30 16:58:03 +0530

    refactored

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ SHEETS_FILE_ID = os.getenv('SHEETS_FILE_ID')
 SHEET_NAME = os.getenv('SHEET_NAME')
 SHEET_ID = int(os.getenv('SHEET_ID'))
 
-COMPANY_NAME_COLUMN = os.getenv('COMPANY_NAME_COLUMN')
-LINKEDIN_PROFILE_COLUMN = os.getenv('LINKEDIN_PROFILE_COLUMN')
-COMPANY_SIZE_COLUMN = os.getenv('COMPANY_SIZE_COLUMN')
-COMPANY_INDUSTRY_COLUMN = os.getenv('COMPANY_INDUSTRY_COLUMN')
+COMPANY_NAME_COLUMN = os.getenv('COMPANY_NAME_COLUMN', 'A')
+LINKEDIN_PROFILE_COLUMN = os.getenv('LINKEDIN_PROFILE_COLUMN', 'B')
+COMPANY_SIZE_COLUMN = os.getenv('COMPANY_SIZE_COLUMN', 'C')
+COMPANY_INDUSTRY_COLUMN = os.getenv('COMPANY_INDUSTRY_COLUMN', 'D')
 
 # LinkedIn config
-LINKEDIN_COOKIES_FILE_NAME = os.getenv('LINKEDIN_COOKIES_FILE_NAME')
+LINKEDIN_COOKIES_FILE_NAME = os.getenv('LINKEDIN_COOKIES_FILE_NAME', 'linkedin_cookies.json')
 LINKEDIN_NOT_LOGGED_IN_PATHS = ['/signup/cold-join', '/signup', '/login', '/authwall']
 
 # Google Auth config
@@ -23,8 +23,9 @@ SCOPES = (
     'https://www.googleapis.com/auth/spreadsheets.readonly',
 )
 
-TOKEN_FILE_PATH = os.getenv('TOKEN_FILE_PATH')
-CREDENTIAL_FILE_PATH = os.getenv('CREDENTIAL_FILE_PATH')
+TOKEN_FILE_PATH = os.getenv('TOKEN_FILE_PATH', 'token.json')
+CREDENTIAL_FILE_PATH = os.getenv('CREDENTIAL_FILE_PATH', 'credentials.json')
 
 # Stat config
-STAT_FILE_NAME = os.getenv('STAT_FILE_NAME')
+STAT_FILE_NAME = os.getenv('STAT_FILE_NAME', 'stat.json')
+DEFAULT_FUNCTION_TIMEOUT = int(os.getenv('DEFAULT_FUNCTION_TIMEOUT', 1000)) # in seconds
